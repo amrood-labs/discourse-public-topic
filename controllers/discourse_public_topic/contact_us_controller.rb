1ae71c7722f1b6737b39f37e3bf8a0d48ec7bbb0
Zaid Akram <hello@zaidakram.com>
2022-03-30 14:39:50 +0500

    Fixed email sending and 'de' locale.

--- controllers/discourse_public_topic/contact_us_controller.rb
+++ controllers/discourse_public_topic/contact_us_controller.rb
@@ -2,9 +2,10 @@ class DiscoursePublicTopic::ContactUsController < ::ApplicationController
   skip_before_action :redirect_to_login_if_required, :check_xhr
 
   def create
-    DiscoursePublicTopic::ContactMailer.notify_about_query(
+    message = DiscoursePublicTopic::ContactMailer.notify_about_query(
       params.permit(:name, :email, :query).to_h
     )
+    Email::Sender.new(message, :someone_contacted_via_contact_us).send
     redirect_to thank_you_path
   end
 end
